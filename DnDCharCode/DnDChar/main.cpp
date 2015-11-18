5b5a26c76075db4d7ee3a6ba5f9439fd809f97f2
NicoleLane <nlane2@ithaca.edu>
2015-11-18 18:37:41 -0500

    Here’s a generic main file I am no longer unique

---
+++ DnDCharCode/DnDChar/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  D&D
+//
+//  Created by Nicole Lane on 11/16/15.
+//  Copyright (c) 2015 Nicole Lane. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
