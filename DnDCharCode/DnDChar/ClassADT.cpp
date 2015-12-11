e61548d27d3dd21c4ecdb592a4a2e3e6038126fe
NicoleLane <nlane2@ithaca.edu>
2015-12-10 22:17:49 -0500

    i still don’t know

--- DnDCharCode/DnDChar/ClassADT.cpp
+++ DnDCharCode/DnDChar/ClassADT.cpp
@@ -17,7 +17,7 @@ int ClassADT::generateRandomNum(int min, int max)
 //Adds a pseudo-random amount of HP to a character
 //Max value is 10 (based on hitDie)
 //Modifier is based on character's Constitution score.
-int ClassADT::rollHP(const int modifier)
+int ClassADT::rollHP(int modifier)
 {
 	return generateRandomNum(1, hitDie) + modifier;
 }
